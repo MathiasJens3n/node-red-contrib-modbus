22cc0e33195ceea683eb872a8fbdeec5ff240ff4
Mathias Jensen <94064263+MathiasJens3n@users.noreply.github.com>
2024-01-11 12:49:52 +0100

    Update modbus-flex-getter.js
    added the option to connect and disconnect

--- src/modbus-flex-getter.js
+++ src/modbus-flex-getter.js
@@ -7,222 +7,209 @@
  */
 
 /**
- * Modbus flexible Getter node.
- * @module NodeRedModbusFlexGetter
+ * Modbus Getter node.
+ * @module NodeRedModbusGetter
  *
  * @param RED
  */
 module.exports = function (RED) {
-  'use strict'
+  "use strict";
   // SOURCE-MAP-REQUIRED
-  const mbBasics = require('./modbus-basics')
-  const mbCore = require('./core/modbus-core')
-  const mbIOCore = require('./core/modbus-io-core')
-  const internalDebugLog = require('debug')('contribModbus:flex:getter')
+  const mbBasics = require("./modbus-basics");
+  const mbCore = require("./core/modbus-core");
+  const mbIOCore = require("./core/modbus-io-core");
+  const internalDebugLog = require("debug")("contribModbus:getter");
 
-  function ModbusFlexGetter (config) {
-    RED.nodes.createNode(this, config)
+  function ModbusGetter(config) {
+    RED.nodes.createNode(this, config);
 
-    this.name = config.name
-    this.showStatusActivities = config.showStatusActivities
-    this.showErrors = config.showErrors
-    this.showWarnings = config.showWarnings
-    this.connection = null
+    this.name = config.name;
+    this.unitid = config.unitid;
 
-    this.useIOFile = config.useIOFile
-    this.ioFile = RED.nodes.getNode(config.ioFile)
-    this.useIOForPayload = config.useIOForPayload
-    this.logIOActivities = config.logIOActivities
+    this.dataType = config.dataType;
+    this.adr = config.adr;
+    this.quantity = config.quantity;
 
-    this.emptyMsgOnFail = config.emptyMsgOnFail
-    this.keepMsgProperties = config.keepMsgProperties
-    this.internalDebugLog = internalDebugLog
-    this.verboseLogging = RED.settings.verbose
+    this.showStatusActivities = config.showStatusActivities;
+    this.showErrors = config.showErrors;
+    this.showWarnings = config.showWarnings;
+    this.msgThruput = config.msgThruput;
+    this.connection = null;
 
-    this.delayOnStart = config.delayOnStart
-    this.startDelayTime = parseInt(config.startDelayTime) || 10
+    this.useIOFile = config.useIOFile;
+    this.ioFile = RED.nodes.getNode(config.ioFile);
+    this.useIOForPayload = config.useIOForPayload;
+    this.logIOActivities = config.logIOActivities;
 
-    const node = this
-    node.bufferMessageList = new Map()
-    node.INPUT_TIMEOUT_MILLISECONDS = 1000
-    node.delayOccured = false
-    node.inputDelayTimer = null
+    this.emptyMsgOnFail = config.emptyMsgOnFail;
+    this.keepMsgProperties = config.keepMsgProperties;
+    this.internalDebugLog = internalDebugLog;
+    this.verboseLogging = RED.settings.verbose;
 
-    mbBasics.setNodeStatusTo('waiting', node)
+    this.delayOnStart = config.delayOnStart;
+    this.startDelayTime = parseInt(config.startDelayTime) || 10;
 
-    const modbusClient = RED.nodes.getNode(config.server)
-    if (!modbusClient) {
-      return
-    }
-    modbusClient.registerForModbus(node)
-    mbBasics.initModbusClientEvents(node, modbusClient)
+    const node = this;
+    node.bufferMessageList = new Map();
+    node.INPUT_TIMEOUT_MILLISECONDS = 1000;
+    node.delayOccured = false;
+    node.inputDelayTimer = null;
 
-    node.onModbusReadDone = function (resp, msg) {
+    mbBasics.setNodeStatusTo("waiting", node);
+
+    let modbusClient;
+
+    node.onModbusCommandDone = function (resp, msg) {
       if (node.showStatusActivities) {
-        mbBasics.setNodeStatusTo('reading done', node)
+        mbBasics.setNodeStatusTo("reading done", node);
       }
-
-      node.send(mbIOCore.buildMessageWithIO(node, resp.data, resp, msg))
-      node.emit('modbusFlexGetterNodeDone')
-    }
+      node.send(mbIOCore.buildMessageWithIO(node, resp.data, resp, msg));
+      node.emit("modbusGetterNodeDone");
+    };
 
     node.errorProtocolMsg = function (err, msg) {
       if (node.showErrors) {
-        mbBasics.logMsgError(node, err, msg)
+        mbBasics.logMsgError(node, err, msg);
       }
-    }
+    };
 
-    node.onModbusReadError = function (err, msg) {
-      node.internalDebugLog(err.message)
-      const origMsg = mbCore.getOriginalMessage(node.bufferMessageList, msg)
-      node.errorProtocolMsg(err, origMsg)
-      mbBasics.sendEmptyMsgOnFail(node, err, msg)
-      mbBasics.setModbusError(node, modbusClient, err, origMsg)
-      node.emit('modbusFlexGetterNodeError')
-    }
-
-    node.prepareMsg = function (msg) {
-      if (typeof msg.payload === 'string') {
-        msg.payload = JSON.parse(msg.payload)
-      }
-
-      msg.payload.fc = parseInt(msg.payload.fc) || 3
-      msg.payload.unitid = parseInt(msg.payload.unitid)
-      msg.payload.address = parseInt(msg.payload.address) || 0
-      msg.payload.quantity = parseInt(msg.payload.quantity) || 1
-
-      return msg
-    }
-
-    node.isValidModbusMsg = function (msg) {
-      let isValid = true
-
-      if (!(Number.isInteger(msg.payload.fc) &&
-              msg.payload.fc >= 1 &&
-              msg.payload.fc <= 4)) {
-        node.error('FC Not Valid', msg)
-        isValid &= false
-      }
-
-      if (isValid &&
-            !(Number.isInteger(msg.payload.address) &&
-            msg.payload.address >= 0 &&
-            msg.payload.address <= 65535)) {
-        node.error('Address Not Valid', msg)
-        isValid &= false
-      }
-
-      if (isValid &&
-            !(Number.isInteger(msg.payload.quantity) &&
-            msg.payload.quantity >= 1 &&
-            msg.payload.quantity <= 65535)) {
-        node.error('Quantity Not Valid', msg)
-        isValid &= false
-      }
-
-      return isValid
-    }
+    node.onModbusCommandError = function (err, msg) {
+      node.internalDebugLog(err.message);
+      const origMsg = mbCore.getOriginalMessage(node.bufferMessageList, msg);
+      node.errorProtocolMsg(err, origMsg);
+      mbBasics.sendEmptyMsgOnFail(node, err, msg);
+      mbBasics.setModbusError(node, modbusClient, err, origMsg);
+      node.emit("modbusGetterNodeError");
+    };
 
     node.buildNewMessageObject = function (node, msg) {
-      const messageId = mbCore.getObjectId()
+      const messageId = mbCore.getObjectId();
       return {
         topic: msg.topic || node.id,
         messageId,
         payload: {
-          value: msg.payload.value || msg.value,
-          unitid: msg.payload.unitid,
-          fc: msg.payload.fc,
-          address: msg.payload.address,
-          quantity: msg.payload.quantity,
-          emptyMsgOnFail: node.emptyMsgOnFail,
-          keepMsgProperties: node.keepMsgProperties,
-          messageId
-        }
-      }
-    }
+          value: msg.payload.value || msg.payload,
+          unitid: node.unitid,
+          fc: mbCore.functionCodeModbusRead(node.dataType),
+          address: node.adr,
+          quantity: node.quantity,
+          messageId,
+        },
+      };
+    };
 
-    function verboseWarn (logMessage) {
+    function verboseWarn(logMessage) {
       if (RED.settings.verbose && node.showWarnings) {
-        node.warn('Flex-Getter -> ' + logMessage)
+        node.warn("Getter -> " + logMessage);
       }
     }
 
     node.isReadyForInput = function () {
-      return (modbusClient.client && modbusClient.isActive() && node.delayOccured)
-    }
+      return (
+        modbusClient.client && modbusClient.isActive() && node.delayOccured
+      );
+    };
 
     node.isNotReadyForInput = function () {
-      return !node.isReadyForInput()
-    }
+      return !node.isReadyForInput();
+    };
 
     node.resetInputDelayTimer = function () {
       if (node.inputDelayTimer) {
-        verboseWarn('reset input delay timer node ' + node.id)
-        clearTimeout(node.inputDelayTimer)
+        verboseWarn("reset input delay timer node " + node.id);
+        clearTimeout(node.inputDelayTimer);
       }
-      node.inputDelayTimer = null
-      node.delayOccured = false
-    }
+      node.inputDelayTimer = null;
+      node.delayOccured = false;
+    };
 
     node.initializeInputDelayTimer = function () {
-      node.resetInputDelayTimer()
+      node.resetInputDelayTimer();
       if (node.delayOnStart) {
-        verboseWarn('initialize input delay timer node ' + node.id)
+        verboseWarn("initialize input delay timer node " + node.id);
         node.inputDelayTimer = setTimeout(() => {
-          node.delayOccured = true
-        }, node.INPUT_TIMEOUT_MILLISECONDS * node.startDelayTime)
+          node.delayOccured = true;
+        }, node.INPUT_TIMEOUT_MILLISECONDS * node.startDelayTime);
       } else {
-        node.delayOccured = true
+        node.delayOccured = true;
       }
-    }
+    };
 
-    node.initializeInputDelayTimer()
+    node.initializeInputDelayTimer();
 
-    node.on('input', function (msg) {
+    node.on("input", function (msg, done) {
+      let connection = msg["payload"]["connection"];
+
+      if (connection === true) {
+        modbusClient = RED.nodes.getNode(config.server);
+        if (!modbusClient) {
+          return;
+        }
+
+        modbusClient.registerForModbus(node);
+        mbBasics.initModbusClientEvents(node, modbusClient);
+        node.status({ fill: "green", shape: "ring", text: "connected" });
+      }
+
+      if (connection === false) {
+        mbBasics.setNodeStatusTo("closed", node);
+        node.bufferMessageList.clear();
+        modbusClient.deregisterForModbus(node.id, done);
+        modbusClient.client.close();
+        node.status({ fill: "red", shape: "ring", text: "disconnected" });
+      }
+
       if (mbBasics.invalidPayloadIn(msg)) {
-        verboseWarn('Invalid message on input.')
-        return
+        verboseWarn("Invalid message on input.");
+        return;
       }
 
       if (node.isNotReadyForInput()) {
-        verboseWarn('Inject while node is not ready for input.')
-        return
+        verboseWarn("Inject while node is not ready for input.");
+        return;
       }
 
       if (modbusClient.isInactive()) {
-        verboseWarn('You sent an input to inactive client. Please use initial delay on start or send data more slowly.')
-        return
+        verboseWarn(
+          "You sent an input to inactive client. Please use initial delay on start or send data more slowly."
+        );
+        return;
       }
 
-      const origMsgInput = Object.assign({}, msg) // keep it origin
+      const origMsgInput = Object.assign({}, msg); // keep it origin
       try {
-        const inputMsg = node.prepareMsg(origMsgInput)
-        if (node.isValidModbusMsg(inputMsg)) {
-          const newMsg = node.buildNewMessageObject(node, inputMsg)
-          node.bufferMessageList.set(newMsg.messageId, mbBasics.buildNewMessage(node.keepMsgProperties, inputMsg, newMsg))
-          modbusClient.emit('readModbus', newMsg, node.onModbusReadDone, node.onModbusReadError)
+        const newMsg = node.buildNewMessageObject(node, origMsgInput);
+        node.bufferMessageList.set(
+          newMsg.messageId,
+          mbBasics.buildNewMessage(node.keepMsgProperties, origMsgInput, newMsg)
+        );
+        modbusClient.emit(
+          "readModbus",
+          newMsg,
+          node.onModbusCommandDone,
+          node.onModbusCommandError
+        );
+
+        if (node.showStatusActivities) {
+          mbBasics.setNodeStatusTo(modbusClient.actualServiceState, node);
         }
       } catch (err) {
-        node.errorProtocolMsg(err, origMsgInput)
-        mbBasics.sendEmptyMsgOnFail(node, err, origMsgInput)
+        node.errorProtocolMsg(err, origMsgInput);
+        mbBasics.sendEmptyMsgOnFail(node, err, origMsgInput);
       }
+    });
 
-      if (node.showStatusActivities) {
-        mbBasics.setNodeStatusTo(modbusClient.actualServiceState, node)
-      }
-    })
-
-    node.on('close', function (done) {
-      node.resetInputDelayTimer()
-      mbBasics.setNodeStatusTo('closed', node)
-      node.bufferMessageList.clear()
-      modbusClient.deregisterForModbus(node.id, done)
-    })
+    node.on("close", function (done) {
+      mbBasics.setNodeStatusTo("closed", node);
+      node.bufferMessageList.clear();
+      modbusClient.deregisterForModbus(node.id, done);
+    });
 
     if (!node.showStatusActivities) {
-      mbBasics.setNodeDefaultStatus(node)
+      mbBasics.setNodeDefaultStatus(node);
     }
+    node.status({ fill: "red", shape: "ring", text: "disconnected" });
   }
 
-  RED.nodes.registerType('modbus-flex-getter', ModbusFlexGetter)
-}
+  RED.nodes.registerType("modbus-getter", ModbusGetter);
+};
